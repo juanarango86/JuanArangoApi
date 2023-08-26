f5644978c7942efbb2d9ebf1d2fe36164036e5b8
jarango <jarango@cfa.com.co>
2023-08-25 23:20:50 -0500

    commit de felicidad
    commit de felicidad 25082023

--- JuanArangoApi/Data/Dto/FormularioDto.cs
+++ JuanArangoApi/Data/Dto/FormularioDto.cs
@@ -5,14 +5,14 @@ namespace JuanArangoApi.Data.Dto
 {
     public class FormularioDto
     {
-        public string Pregunta_1 { get; set; }
-        public string Respuesta_1 { get; set; }
-        public string Pregunta_2 { get; set; }
+        public string Pregunta_1 { get; set; } = string.Empty;
+        public string Respuesta_1 { get; set; } 
+        public string Pregunta_2 { get; set; } = string.Empty;
         public string Respuesta_2 { get; set; }
-        public string Pregunta_3 { get; set; }
+        public string Pregunta_3 { get; set; } = string.Empty;
         public string Respuesta_3 { get; set; }
-        public string Latitud { get; set; }
-        public string Longitud { get; set; }
+        public string Latitud { get; set; } = string.Empty;
+        public string Longitud { get; set; } = string.Empty;
         public long UserId { get; set; }
 
     }
