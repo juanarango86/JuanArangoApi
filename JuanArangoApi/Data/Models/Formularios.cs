0de870c164df0c6ed4d14d291fc8353b247396fb
jarango <jarango@cfa.com.co>
2023-08-25 22:58:36 -0500

    form post
     25082023 form post feat

--- JuanArangoApi/Data/Models/Formularios.cs
+++ JuanArangoApi/Data/Models/Formularios.cs
@@ -10,30 +10,30 @@ namespace JuanArangoApi.Data.Models
         [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public long IdFormulario { get; set; }
 
-        [Required]
+        
         public string Pregunta_1 { get; set; }
 
-        [Required]
+        
         public string Respuesta_1 { get; set; }
 
-        [Required]
+        
         public string Pregunta_2 { get; set; }
 
-        [Required]
+        
         public string Respuesta_2 { get; set; }
 
-        [Required]
+        
         public string Pregunta_3 { get; set; }
 
-        [Required]
+        
         public string Respuesta_3 { get; set; }
 
        public DateTime FechaEncuesta { get; set; } = DateTime.Now;
 
-        [Required]
+        
         public string Latitud { get; set; }
 
-        [Required]
+        
         public string Longitud { get; set; }
 
         public long UserId { get; set; }
